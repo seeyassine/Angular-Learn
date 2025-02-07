035290aedb3b1f798558a1f73b2af229711f7449
See Yassine <seeyassine@gmail.com>
2025-02-07 16:30:04 +0100

    If condition old and new syntax

---
+++ src/app/components/directives/directives.component.ts
@@ -0,0 +1,20 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './directives.component.html',
+  styleUrl: './directives.component.css'
+})
+export class DirectivesComponent {
+
+  Isloggedin: boolean = false;
+  username:string = 'logged in';
+
+  handleClick() {
+    this.Isloggedin = !this.Isloggedin
+    }
+
+}
