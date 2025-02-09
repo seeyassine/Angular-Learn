0029a3d0a341a02c182193f1cdac3e26f246858f
See Yassine <seeyassine@gmail.com>
2025-02-09 23:13:00 +0100

    88 - ParentChild Data Flow I

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,NavbarComponent],
+  imports: [RouterOutlet, HeaderComponent,NavbarComponent,PostsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular-learn';
+ isLoggedIn: boolean = false;
+
+ postTitle: string = 'Post 1';
 }
