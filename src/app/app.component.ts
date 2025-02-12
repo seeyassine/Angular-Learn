f7d29022415ccb9333a771327888c121a62c9154
See Yassine <seeyassine@gmail.com>
2025-02-12 11:27:32 +0100

    1. Understanding FormControl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControlComponent } from './form-control/form-control.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormControlComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/form-control/form-control.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-control',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './form-control.component.html',
+  styleUrl: './form-control.component.css'
+})
+export class FormControlComponent {
+  usernameControl = new FormControl('',[
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(10),
+    Validators.pattern('^[a-zA-Z0-9]+$') //alpha nimeruc only 
+  ]);
+
+  showValue(){
+      console.log('value:', this.usernameControl.value);
+      console.log('Validation status:' , this.usernameControl.valid);
+      console.log(this.usernameControl.errors);
+  }
+
+}
