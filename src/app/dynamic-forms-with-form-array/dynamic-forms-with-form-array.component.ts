6668728aff10a679d03e255ebc2b855e9d60c39a
See Yassine <seeyassine@gmail.com>
2025-02-12 13:58:36 +0100

    form builder

--- src/app/dynamic-forms-with-form-array/dynamic-forms-with-form-array.component.ts
+++ src/app/dynamic-forms-with-form-array/dynamic-forms-with-form-array.component.ts
@@ -1,6 +1,6 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { Form, FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Form, FormArray, FormGroup, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-forms-with-form-array',
@@ -13,28 +13,33 @@ export class DynamicFormsWithFormArrayComponent {
 
   employeeForm: FormGroup;
 
-  constructor(){
-    this.employeeForm = new FormGroup({
-      employees: new FormArray([]),
+  constructor(private fb: FormBuilder){
+    this.employeeForm = this.fb.group({
+      employees: this.fb.array([]),
 
     })
   }
 
+   // Getter pour accéder facilement au FormArray
   get employees(): FormArray{
     return this.employeeForm.get('employees') as FormArray;
   }
 
 
   addEmployee(): void{
-      const employeeGroup = new FormGroup({
-        name: new FormControl(''),
-        job: new FormControl(''),
+      const employeeGroup = this.fb.group({
+        name: ['', Validators.required],
+        job: ['', Validators.required]
       });
       this.employees.push(employeeGroup)
   }
 
   submitForm(){
       console.log(this.employeeForm.value);
+  }
+  
+  removeEmployee(index: number): void {
+    this.employees.removeAt(index);
   }
   
 }
