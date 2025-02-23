e8d636d14431a3dcfd76287a2978195f847e1946
See Yassine <seeyassine@gmail.com>
2025-02-23 23:11:13 +0000

    The importante of interface

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
+interface Post {
+  id: number,
+  title: string,
+  post: string
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  posts: Array<any> = [
+  posts: Array<Post> = [
     {id:1, title: 'title1' , post:'seeyassine@gmail.com'},
     {id:2, title: 'title2' , post:'yassine@gmail.com'},
   ]
